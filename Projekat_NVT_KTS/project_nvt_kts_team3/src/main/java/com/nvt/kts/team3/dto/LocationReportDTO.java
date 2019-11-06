4d185ad1f64bb2118b8d948190123d191976f35d
LeonaNedeljkovic <leona.nedeljkovic@gmail.com>
2019-11-06 22:18:53 +0100

    reports

---
+++ Projekat_NVT_KTS/project_nvt_kts_team3/src/main/java/com/nvt/kts/team3/dto/LocationReportDTO.java
@@ -0,0 +1,66 @@
+package com.nvt.kts.team3.dto;
+
+import java.util.ArrayList;
+
+public class LocationReportDTO {
+	private ArrayList<String> dailyLabels = new ArrayList<>();
+	private ArrayList<Integer> dailyValues = new ArrayList<>();
+	private ArrayList<String> weeklyLabels = new ArrayList<>();
+	private ArrayList<Integer> weeklyValues = new ArrayList<>();
+	private ArrayList<String> monthlyLabels = new ArrayList<>();
+	private ArrayList<Integer> monthlyValues = new ArrayList<>();
+	
+	public LocationReportDTO() {
+		super();
+	}
+	public LocationReportDTO(ArrayList<String> dailyLabels, ArrayList<Integer> dailyValues,
+			ArrayList<String> weeklyLabels, ArrayList<Integer> weeklyValues, ArrayList<String> monthlyLabels,
+			ArrayList<Integer> monthlyValues) {
+		super();
+		this.dailyLabels = dailyLabels;
+		this.dailyValues = dailyValues;
+		this.weeklyLabels = weeklyLabels;
+		this.weeklyValues = weeklyValues;
+		this.monthlyLabels = monthlyLabels;
+		this.monthlyValues = monthlyValues;
+	}
+	public ArrayList<String> getDailyLabels() {
+		return dailyLabels;
+	}
+	public void setDailyLabels(ArrayList<String> dailyLabels) {
+		this.dailyLabels = dailyLabels;
+	}
+	public ArrayList<Integer> getDailyValues() {
+		return dailyValues;
+	}
+	public void setDailyValues(ArrayList<Integer> dailyValues) {
+		this.dailyValues = dailyValues;
+	}
+	public ArrayList<String> getWeeklyLabels() {
+		return weeklyLabels;
+	}
+	public void setWeeklyLabels(ArrayList<String> weeklyLabels) {
+		this.weeklyLabels = weeklyLabels;
+	}
+	public ArrayList<Integer> getWeeklyValues() {
+		return weeklyValues;
+	}
+	public void setWeeklyValues(ArrayList<Integer> weeklyValues) {
+		this.weeklyValues = weeklyValues;
+	}
+	public ArrayList<String> getMonthlyLabels() {
+		return monthlyLabels;
+	}
+	public void setMonthlyLabels(ArrayList<String> monthlyLabels) {
+		this.monthlyLabels = monthlyLabels;
+	}
+	public ArrayList<Integer> getMonthlyValues() {
+		return monthlyValues;
+	}
+	public void setMonthlyValues(ArrayList<Integer> monthlyValues) {
+		this.monthlyValues = monthlyValues;
+	}
+
+	
+
+}
