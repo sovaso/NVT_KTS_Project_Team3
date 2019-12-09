a3e9b6425005c9bc54ef44d36237d065695ac810
MarinaVojnovic <marina.vojnovic1997@gmail.com>
2019-12-09 15:07:53 +0100

    Location service integration test

--- Projekat_NVT_KTS/project_nvt_kts_team3/src/test/java/com/nvt/kts/team3/serviceTests/LocationServiceIntegrationTest.java
+++ Projekat_NVT_KTS/project_nvt_kts_team3/src/test/java/com/nvt/kts/team3/serviceTests/LocationServiceIntegrationTest.java
@@ -1,5 +1,39 @@
 package com.nvt.kts.team3.serviceTests;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.nvt.kts.team3.model.Event;
+import com.nvt.kts.team3.model.EventType;
+import com.nvt.kts.team3.repository.EventRepository;
+import com.nvt.kts.team3.repository.ReservationRepository;
+import com.nvt.kts.team3.service.EventService;
+import com.nvt.kts.team3.service.LocationService;
+import com.nvt.kts.team3.service.LocationZoneService;
+import com.nvt.kts.team3.service.MaintenanceService;
+import com.nvt.kts.team3.service.TicketService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class LocationServiceIntegrationTest {
 
+	@Autowired
+	private LocationService locationService;
 }
